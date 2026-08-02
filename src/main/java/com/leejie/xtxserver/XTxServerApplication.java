327e495e4c54a6a55862fd11bf7b38707c7522e6
leejie <leejieqaq@163.com>
2026-08-02 15:42:30 +0800

    first commit

---
+++ src/main/java/com/leejie/xtxserver/XTxServerApplication.java
@@ -0,0 +1,13 @@
+package com.leejie.xtxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XTxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XTxServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leejie/xtxserver/XTxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leejie.xtxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XTxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
